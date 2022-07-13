22eff6fe049712e040a84d13f23bc56f1b38b6ff
Liam Brookfield <lajbrookfield@hotmail.com>
2022-07-13 22:35:28 +0100

    Added number generator to top of gameboard

--- Mainscript.py
+++ Mainscript.py
@@ -8,9 +8,11 @@ class GameBoard:
     col_walls = "|   "
     play_storer = "|   "
         
-    def make_board(self, column_num, row_num):
-        
-        for i in range(row_num):
+    def make_board(self, column_num = 7, row_num = 6):
+        for c in range(column_num):
+            print(f"  {c+1} ", end = "")
+        print("")
+        for r in range(row_num):
             print(self.row_divider * (column_num - 1) + self.row_divider_end)
             print(self.col_walls * (column_num + 1))
             print(self.play_storer * (column_num + 1))
@@ -19,4 +21,4 @@ class GameBoard:
     
 
 board1 = GameBoard()
-board1.make_board(4, 4)
+board1.make_board()
