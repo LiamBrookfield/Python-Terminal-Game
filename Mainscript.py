23ae3bba1664fe373966f8fdf6ebf6b57880d929
Liam Brookfield <lajbrookfield@hotmail.com>
2022-07-13 21:18:54 +0100

    made gameboard class

--- Mainscript.py
+++ Mainscript.py
@@ -1,1 +1,26 @@
-print("eyo"
+#Basic Version to conceptualize game
+grid = {1: "XXXXX", 2: "XXXXX", 3: "XXXXX", 4: "XXXXX", 5: "XXXXX", }
+
+for cell in grid:
+    print(grid.get(cell))
+
+
+
+
+# Idea for gameboard constructor
+# class GameBoard:
+#     cell = """
+#         +---
+#         |   
+#         |   
+#         |   
+#         +---
+#         """
+    
+    
+#     def __init__(self, row_count, column_count):
+        
+      
+# board1 = GameBoard()
+        
+# print(board1.cell)
