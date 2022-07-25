fc520c25a2dd87b72aa1d0fa61633bd981ab5666
Liam Brookfield <lajbrookfield@hotmail.com>
2022-07-26 00:19:58 +0100

    Converted dictionaries to lists for sake of sanity

--- Mainscript.py
+++ Mainscript.py
@@ -8,22 +8,22 @@ class Connectfour:
     x_piece = "| X "
     o_piece = "| O "
 
-    game_dict = {1: {1: "|a  ", 2: "|   ", 3: "|   ", 4: "|   ", 5: "|   ", 6: "|   ", 7: "|   ", 8: "|"},
-        2: {1: "|b  ", 2: "|   ", 3: "|   ", 4: "|   ", 5: "|   ", 6: "|   ", 7: "|   ", 8: "|"},
-        3: {1: "|c  ", 2: "|   ", 3: "|   ", 4: "|   ", 5: "|   ", 6: "|   ", 7: "|   ", 8: "|"},
-        4: {1: "|d  ", 2: "|   ", 3: "|   ", 4: "|   ", 5: "|   ", 6: "|   ", 7: "|   ", 8: "|"},
-        5: {1: "|e  ", 2: "|   ", 3: "|   ", 4: "|   ", 5: "|   ", 6: "|   ", 7: "|   ", 8: "|"},
-        6: {1: "|f  ", 2: "|   ", 3: "|   ", 4: "|   ", 5: "|   ", 6: "|   ", 7: "|   ", 8: "|"}}
+    game_dict = [["|a  ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|"],
+        ["|b  ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|"],
+        ["|c  ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|"],
+        ["|d  ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|"],
+        ["|e  ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|"],
+        ["|f  ", "|   ", "|   ", "|   ", "|   ", "|   ", "|   ", "|"]]
 
 
     
     def print_current_board(self):
-        dict_string1 = "".join(self.game_dict.get(1).values())
-        dict_string2 = "".join(self.game_dict.get(2).values())
-        dict_string3 = "".join(self.game_dict.get(3).values())
-        dict_string4 = "".join(self.game_dict.get(4).values())
-        dict_string5 = "".join(self.game_dict.get(5).values())
-        dict_string6 = "".join(self.game_dict.get(6).values())
+        dict_string1 = "".join(self.game_dict[0])
+        dict_string2 = "".join(self.game_dict[1])
+        dict_string3 = "".join(self.game_dict[2])
+        dict_string4 = "".join(self.game_dict[3])
+        dict_string5 = "".join(self.game_dict[4])
+        dict_string6 = "".join(self.game_dict[5])
         self.board = (f"""  1   2   3   4   5   6   7
 {self.row_divider}
 {self.col_walls}
@@ -54,12 +54,11 @@ class Connectfour:
         print(self.board)
 
 
-    def make_play(self, piece):
-       for i in self.game_dict:
-        for subdict in (self.game_dict.get(i)).values():
-            if subdict == "|   ":
-                subdict = piece
-                print(subdict)
+    def make_play(self):
+        for lst in self.game_dict:
+            print(lst)
+          
+            
             
             
     
@@ -73,6 +72,5 @@ class Connectfour:
         
         
 board1 = Connectfour()
-
-board1.make_play("| X ")
 board1.print_current_board()
+board1.make_play()
